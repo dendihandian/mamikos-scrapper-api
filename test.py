004098a142a5e5f302a1d0671e3d6e8329249e54
Dendi Handian <dendihandian@gmail.com>
2021-02-06 17:38:44 +0700

    first successful test

---
+++ test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+
+driver.get('https://www.mamikos.com/area')
+
+f = open('result.html', 'w')
+f.write(driver.page_source)
+f.close()
+
+# print(driver.page_source)
